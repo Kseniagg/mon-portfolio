9830aa219ee77f7bb187562e3a164dd96df0eef0
Kseniia Gladkova <gladkowaks@gmail.com>
2023-09-22 16:24:57 +0200

    update the list of projects

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -2,7 +2,7 @@ import project01 from "./../img/projects/01.jpg";
 
 const projects = [
     {
-        title: 'Nourir mon ami',
+        title: 'Nourrir mon ami',
         skills: 'React, Node.js, MySQL',
         img: project01,
         gitHubLink: 'https://github.com/Kseniagg/nourrirmonami',
