f6b1aadd70baabb57254b74c73334795ae1a6e84
Kseniia Gladkova <gladkowaks@gmail.com>
2023-09-22 16:17:17 +0200

    update Contacts

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -7,7 +7,7 @@ const Contacts = () => {
                 <ul className="content-list">
                     <li className="content-list__item">
                         <h2 className="title-2">
-                            Emplacement</h2>
+                            Adresse</h2>
                         <p>Meudon, 92190</p>
                     </li>
                     <li className="content-list__item">
