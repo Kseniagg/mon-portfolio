49dc95266953b1f98dad024b66c36f1fce5ac2f9
Kseniia Gladkova <gladkowaks@gmail.com>
2023-10-04 15:12:36 +0200

    adds information to the footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,6 +1,4 @@
 import "./style.css";
-/* import github from "./../../img/icons/gitHub.svg";
-import linkedin from "./../../img/icons/linkedIn.svg"; */
 
 const Footer = () => {
     return (
@@ -8,6 +6,7 @@ const Footer = () => {
             <div className="container">
                 <div className="footer__wrapper">
                     <div className="copyright">
+                        <p className="badge"><a href="https://www.websitecarbon.com/" target="_blank">Cette page a été chargée en 0,7 seconde et n'a émis que 0,05 g de CO2</a></p>
                         <p>© 2023 Kseniia Gladkova</p>
                     </div>
                 </div>
